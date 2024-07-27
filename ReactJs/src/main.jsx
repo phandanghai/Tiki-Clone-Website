4348de247cf2be4d6af01ed6480dc516a3e03ed8
phandanghai <95266476+phandanghai@users.noreply.github.com>
2024-07-27 11:53:26 +0700

    Update main.jsx

--- ReactJs/src/main.jsx
+++ ReactJs/src/main.jsx
@@ -5,11 +5,13 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../src/redux/stores/stores';
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <GoogleOAuthProvider clientId="993152484598-650tli4tbi8lgp6q783evdn1l11ttdcb.apps.googleusercontent.com">
       <App />
+        </GoogleOAuthProvider>
     </PersistGate>
   </Provider>,
 );
